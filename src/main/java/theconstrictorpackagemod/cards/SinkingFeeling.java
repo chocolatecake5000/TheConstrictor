24f2ed8da9f7521e8c6700b17a5b9b7cbed804ae
Cany0udance <blakkrum@aol.com>
2024-05-15 02:46:46 -0400

    slack tide and sinking feeling reworks

--- src/main/java/theconstrictorpackagemod/cards/SinkingFeeling.java
+++ src/main/java/theconstrictorpackagemod/cards/SinkingFeeling.java
@@ -3,8 +3,7 @@ package theconstrictorpackagemod.cards;
 import characterclass.MyCharacter;
 import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.GraveField;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -19,7 +18,7 @@ public class SinkingFeeling extends BaseCard {
     private final static CardInfo cardInfo = new CardInfo(
             "SinkingFeeling",
             2,
-            CardType.ATTACK,
+            CardType.SKILL,
             CardTarget.ALL,
             CardRarity.BASIC,
             MyCharacter.Enums.CARD_COLOR);
@@ -35,26 +34,18 @@ public class SinkingFeeling extends BaseCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         Iterator<AbstractMonster> var3 = AbstractDungeon.getCurrRoom().monsters.monsters.iterator();
-        int baseDamage = 0; // This is a new base damage, adjust as needed for balance.
 
         while (var3.hasNext()) {
             AbstractMonster mo = var3.next();
-            int totalDamage = baseDamage;
 
             if (mo.hasPower(ConstrictingPower.POWER_ID)) {
                 int constrictAmount = mo.getPower(ConstrictingPower.POWER_ID).amount;
-                totalDamage += constrictAmount * this.magicNumber; // Add the damage based on Constricting.
+                int hpLossAmount = constrictAmount * this.magicNumber; // Calculate HP loss as twice the Constricting amount.
+
+                this.addToBot(new LoseHPAction(mo, p, hpLossAmount, AbstractGameAction.AttackEffect.POISON)); // Apply HP loss directly, ignoring block and modifiers.
             }
-
-            // Create a temporary DamageInfo object for modifying the damage based on player's buffs/debuffs.
-            DamageInfo info = new DamageInfo(p, totalDamage, DamageInfo.DamageType.NORMAL);
-            info.applyPowers(p, mo); // This method adjusts the damage based on player and monster powers.
-
-            // Now use the possibly modified damage from the info object
-            this.addToBot(new DamageAction(mo, info, AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         }
     }
-
 
 
     public void upgrade() {
--- src/main/java/theconstrictorpackagemod/cards/SlackTide.java
+++ src/main/java/theconstrictorpackagemod/cards/SlackTide.java
@@ -3,6 +3,7 @@ package theconstrictorpackagemod.cards;
 
 import basemod.helpers.dynamicvariables.MagicNumberVariable;
 import characterclass.MyCharacter;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,6 +11,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.ThornsPower;
 import com.megacrit.cardcrawl.powers.watcher.FreeAttackPower;
+import theconstrictorpackagemod.powers.ConstrictingPower;
 import theconstrictorpackagemod.util.CardInfo;
 
 import static theconstrictorpackagemod.theconstrictormod.makeID;
@@ -25,10 +27,8 @@ public class SlackTide extends BaseCard {
 
     public static final String ID = makeID(cardInfo.baseId);
 
-
-    private static final int BLOCK = 8;
+    private static final int BLOCK = 10;  // Updated block value
     private static final int UPG_BLOCK = 3;
-
 
     public SlackTide() {
         super(cardInfo);
@@ -37,7 +37,22 @@ public class SlackTide extends BaseCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        int currentBlock = p.currentBlock; // Capture the current block before gaining more
+
+        // Gain block first
         addToBot(new GainBlockAction(p, p, block));
-        this.addToBot(new ApplyPowerAction(p, p, new FreeAttackPower(p, 1), 1));
+
+        // Apply negative Constricting based on block gained
+        addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                int blockGained = p.currentBlock - currentBlock; // Calculate the actual block gained
+                if (blockGained > 0) {
+                    // Apply negative Constricting equal to the block gained
+                    addToBot(new ApplyPowerAction(p, p, new ConstrictingPower(p, p, -blockGained), -blockGained));
+                }
+                isDone = true;
+            }
+        });
     }
 }
